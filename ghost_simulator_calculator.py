2120ba0c9c09d7d419344bd38e8dbf6002a84eaf
Alvar Eklind <alvar.eklind@gmail.com>
2025-10-10 08:02:44 +0200

    finish up the bosses

--- ghost_simulator_calculator.py
+++ ghost_simulator_calculator.py
@@ -1,5 +1,5 @@
 #List of bosses with set spawn orders
-boss_list = ["dino king", "grim", "jolly roger", "anomaly", "anonymous", "lord of luck", "spade harbringer", "lord of luck", "remnant dino king", "remnant jolly roger", "remnant anonymous"]
+boss_list = ["dino king", "grim", "jolly roger", "anomaly", "anonymous", "lord of luck", "spade harbringer"]
 
 #Used for for-loops
 number = 0
@@ -9,6 +9,9 @@ dino_king_spawn_locations = ["junkyard", "desert", "beach"]
 grim_spawn_locations = ["board shop", "above spawn", "soul converter"]
 jolly_roger_locations = ["castle bridge", "pixie island bridge", "middle bridge"]
 anomaly_spawn_locations = ["swamplands behind gab3", "farm", "spaceship at the highest point", "data stream next to key fragment", "winter tundra entrance to mushroom forest", "mushroom forest under green ring", "winter tundra on top of bridge", "winter tundra on top of cliff", "data stream next to rowan", "twisting river", "twisting river", "barn above juliana", "spaceship near gum machine", "spaceship near barn entrance", "bloxbyte hq to the right", "spaceship next to billy's fork", "spaceship next to billy's fork", "twisting river", "barn on the floor in a corner", "data stream next to rowan's flashdrive", "barn in fron of julianna", "barn next to rich-ard's tie", "twisting river", "reverse city central square", "reverse city 3rd floor", "reverse city bank entrance", "bloxbyte hq left room", "spaceship below billy's pitchfork", "swamplands next to red ring", "mushroom forest log at the start of obby"]
+anonymous_spawn_locations = ["data stream", "winter tundra", "farm"]
+lord_of_luck_spawn_locations = ["thexz's island", "covenk's island", "makkiemon's island", "goro7's island", "didi1147's island inside pyramid", "castle right side", "castle left side"]
+spade_harbringer_spawn_locations = ["castle gardens", "crystal canyon", "enchanted jungle", "underwater ruins", "fantasy courtyard", "phantom castle", "scrap city", "blue forest", "floating bridge", "hidden caverns"]
 
 #functions
 def boss_calculator(spawn_locations, number):
@@ -34,3 +37,9 @@ if What_to_calculate.lower() == "boss":
     elif what_boss.lower() == "anomaly":
         print("Note that the program will always say that the next spawn location is the same if its a duplicated spawn even if its the ladder of the two")
         boss_calculator(anomaly_spawn_locations, number)
+    elif what_boss.lower() == "anonymous":
+        boss_calculator(anonymous_spawn_locations, number)
+    elif what_boss.lower() == "lord of luck":
+        boss_calculator(lord_of_luck_spawn_locations, number)
+    elif what_boss.lower() == "spade harbringer":
+        boss_calculator(spade_harbringer_spawn_locations, number)
